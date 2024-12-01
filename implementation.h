336428d25201e33e7067873fa6e70dacf850aeac
djbooth-3 <djbooth@miners.utep.edu>
2024-11-30 17:39:24 -0700

    Updated

--- implementation.h
+++ implementation.h
@@ -10,6 +10,41 @@ typedef struct {
   __myfs_off_t next_space;
 } allocated_node_t;
 
+typedef struct {
+  uint32_t magic;
+  size_t size;
+  __myfs_off_t root_dir;
+  __myfs_off_t free_memory;
+} myfs_handle_t;
+
+typedef struct {
+  size_t size;
+  size_t allocated;
+  __myfs_off_t data;
+  __myfs_off_t next_file_block;
+} myfs_file_block_t;
+
+typedef struct {
+  size_t total_size;
+  __myfs_off_t first_file_block;  
+} file_t;
+
+typedef struct {
+  size_t max_children;
+  __myfs_off_t children;
+} directory_t;
+
+typedef struct {
+  char name[(size_t)256];
+  char is_file;
+  struct timespec times[2];
+  union {
+    file_t file;
+    directory_t directory;
+  } type;
+} node_t;
+
+
 void *off_to_ptr(void *fsptr, __myfs_off_t offset);
 __myfs_off_t ptr_to_off(void *fsptr, void *ptr);
 void *get_free_memory_ptr(void *fsptr);
